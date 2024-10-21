9492c6d93e0fe2d994cef132653800745999ce19
CONTENTEvoid <148373198+CONTENTEvoid@users.noreply.github.com>
2024-10-21 16:48:40 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
-function showTest() {
-    alert("Este é um teste fictício do serviço IPTV, apenas para fins educacionais.");
+function showPromotion() {
+    alert("Promoção especial: Experimente grátis por 7 dias e economize em planos anuais!");
 }
