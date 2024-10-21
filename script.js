eb21c2d017a704571d418d194f66e58cc61239f6
CONTENTEvoid <148373198+CONTENTEvoid@users.noreply.github.com>
2024-10-21 16:52:23 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 function showPromotion() {
-    alert("Promoção especial: Experimente grátis por 7 dias e economize em planos anuais!");
+    alert("Promoção Especial: Teste Grátis por 7 dias. Aproveite e descubra os benefícios do Stream Vision!");
 }
